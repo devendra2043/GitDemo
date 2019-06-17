e33c9c3e43096a80bb738183cd77515dd6d9f548
devendra2043 <devendra2043@gmail.com>
2019-06-17 13:40:15 +0530

    commit from gitDemo

--- SessionManagement/src/java/comm/dev/Controller.java
+++ SessionManagement/src/java/comm/dev/Controller.java
@@ -33,6 +33,7 @@ public class Controller extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        System.out.println("GitHub");
        PrintWriter out = response.getWriter();
        HttpSession session= request.getSession();
        String un = request.getParameter("username");
